d1c2c020b64a5cdabb744ba4c98ace99d7dca5af
Ahmed-khlch <55856788+Ahmed-khlch@users.noreply.github.com>
2020-04-15 20:22:13 +0200

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,4 @@
+age = input('how old are you : ')
+newAge = int(age)+10
+print('your age after 10 years is '+ str(newAge) +' years old')
+raw_input(age)
---
+++ 05.py
@@ -0,0 +1,2 @@
+inString = input("input the dtring that you want to evaluate: ")
+print("the string has", len(inString), "characters")
