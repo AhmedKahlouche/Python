f6f18c2b34456922165c5c641b1eeea05cb6fe2a
AhmedKahlouche <ahmed.khlch@gmail.com>
2020-04-19 17:50:02 +0200

    Commit #5 : reorginizing files & started working on Web map prog

--- Dictionary/Prog.py
+++ Dictionary/Prog.py
@@ -20,17 +20,16 @@ def getDef(word,i=0):
                 print(str(idx+1)+". "+get_close_matches(word,data.keys())[idx])
             getDef(word,int(input("choose the number of desired word, or -1 to exit: ")))
         else:
-            return "Word does not exist"
+            return "Word does not exist, bye idiot"
     elif i <= -1 :
         print("exiting the program, goodbye")
     else :
         print("you have chossen the word:",get_close_matches(word,data.keys())[i-1],
-              "here are its deffinitions")
+              ", here are its deffinitions")
         for idx in range(data[get_close_matches(word,data.keys())[i-1]].__len__()):
-            print(str(idx+1)+". "+ data[get_close_matches(word,data.keys())[i-1]][idx] )
-
-
+            print(str(idx+1)+". "+ data[get_close_matches(word,data.keys())[i-1]][idx])
+        return
 # this part of the code is only run if the program is executed as a script
 if __name__ == "__main__":
     print(getDef(input("Enter a Word: ")))
-    print("this is it, have fun idiot")
+    # print("this is it, have fun idiot")
